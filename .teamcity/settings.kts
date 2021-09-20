408d88fae1baa3d2c8c132ac460e04ce1e5f5081
63282359+iyanke <63282359+iyanke@TeamCity>
2021-09-20 21:34:13 +0300

    TeamCity change in 'Ftp Upload Dsl' project: runners of 'Build1' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -77,13 +77,12 @@ object Build1 : BuildType({
         ftpUpload {
             targetUrl = "ftp://172.31.131.24:22"
             securityMode = FTPUpload.SecurityMode.FTPS
-            authMethod = usernameAndPassword {
-                username = "jetbrains"
-                password = "credentialsJSON:3a18efca-ee26-4fc5-ab03-1ba28a703208"
-            }
+            authMethod = anonymous()
             ftpMode = FTPUpload.FTPMode.ACTIVE
             transferMode = FTPUpload.TransferMode.BINARY
             sourcePath = "11.png"
+            param("jetbrains.buildServer.deployer.username", "jetbrains")
+            param("secure:jetbrains.buildServer.deployer.password", "credentialsJSON:3a18efca-ee26-4fc5-ab03-1ba28a703208")
         }
     }
 
